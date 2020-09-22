af3287fa53af00f6cb243143f7465a98994267bd
BronwenM <39170856+BronwenM@users.noreply.github.com>
2020-09-22 09:46:36 -0700

    Add files via upload

---
+++ Scripts/FoodControl.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FoodControl : MonoBehaviour
+{
+
+
+    private GameManager gameManager;
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameManager = FindObjectOfType<GameManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    //private void OnCollisionEnter2D(Collision2D collision)
+    //{
+    //    if (collision.gameObject.tag == "destroy")
+    //    {
+    //        Debug.Log("Squish");
+    //        Destroy(gameObject);
+    //    }
+
+    //}
+
+    //private void OnBecameInvisible()
+    //{
+    //    Destroy(gameObject);
+    //}
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("destroy"))
+        {
+            if (!gameObject.CompareTag("toDestroy"))
+            {
+                gameManager.squashCorrect(false);
+                //gameManager.pointsCalculator(gameManager.destG);
+                Destroy(gameObject);  
+            }
+            if (gameObject.CompareTag("toDestroy"))
+            {
+               
+                gameManager.squashCorrect(true);
+                Destroy(gameObject);
+            }
+        }
+        if (collision.gameObject.CompareTag("basket"))
+        {
+            Debug.Log("Collided w/ Basket");
+            if (!gameObject.CompareTag("toDestroy"))
+            {
+                gameManager.pointsCalculator(gameManager.keepG);
+                Destroy(gameObject);
+            }
+            if (gameObject.CompareTag("toDestroy"))
+            {
+                gameManager.squashCorrect(false);
+                gameManager.pointsCalculator(50);
+                Destroy(gameObject);
+            }
+        }
+    }
+
+    private void OnBecameInvisible()
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ Scripts/MM_Buttons.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MM_Buttons : MonoBehaviour
+{
+	public bool start;
+	public bool quit;
+	public bool htp;
+	public bool x;
+
+	public GameObject htplay;
+
+	public AudioClip click;
+	AudioSource source;
+
+	public Transitions transitions;
+
+	// Start is called before the first frame update
+	void Start()
+	{
+		source = gameObject.AddComponent<AudioSource>();
+		gameObject.GetComponent<TextMesh>().color = Color.white;
+		htplay.SetActive(false);
+	}
+
+	private void Update()
+	{
+
+	}
+
+	void OnMouseEnter()
+	{
+		source.PlayOneShot(click, 1.0f);
+		gameObject.GetComponent<TextMesh>().color = Color.HSVToRGB(239,91,100);
+	}
+
+	void OnMouseExit()
+	{
+		gameObject.GetComponent<TextMesh>().color = Color.white;
+	}
+
+	void OnMouseUp()
+	{
+		if (!source.isPlaying)
+		{
+			if (start)
+			{
+				source.PlayOneShot(click, 5.0f);
+				transitions.TransitionToLevel(1);
+				Debug.Log("lvl1");
+			}
+			if (quit)
+			{
+				source.PlayOneShot(click, 5.0f);
+				Application.Quit();
+				Debug.Log("quit");
+			}
+			if (htp)
+			{
+				source.PlayOneShot(click, 5.0f);
+				Debug.Log("htp");
+				htplay.SetActive(true);
+			}
+			if (x)
+			{
+				source.PlayOneShot(click, 5.0f);
+				htplay.SetActive(false);
+			}
+		}
+		
+	}
+}
